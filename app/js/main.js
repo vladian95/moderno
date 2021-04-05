7d1e98f6192eb68b7bfbb82353db6217b5db4e30
vladian95 <kvankvan1995@gmail.com>
2021-04-05 13:52:44 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function(){
 
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
+
+    var mixer = mixitup('.products__inner-box');
 
 
 
