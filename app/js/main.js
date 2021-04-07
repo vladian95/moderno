46f2f92b0022639655ce112b90923bc5bf3f6a50
vladian95 <kvankvan1995@gmail.com>
2021-04-07 12:47:56 +0300

    added section new items-1

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,15 @@ $(function(){
         slidesToScroll: 4
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
 
     var mixer = mixitup('.products__inner-box');
 
